9539178e4546f94f1e45e7328384e011f7fda00f
MathNinja26 <Estherdentel@gmail.com>
2025-04-10 19:23:03 -0400

    Restore '4/10/2025, 6:40:55 PM Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts'

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,14 +4,6 @@ namespace myTiles {
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile5 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -73,7 +65,7 @@ namespace myTiles {
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,sprites.castle.tileGrass3,sprites.castle.tileGrass1,sprites.builtin.forestTiles8], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100001010101010101010101010101010101010404040404040404040404040404010104040404040404040204040402040101040402040404040404040404040401010404040404040404040404040404010104040404040204040404040404040101040404040404040404020404040401010404040204040404040404040404010104040404040404040404040404040101040404040404040404040404040401010404040404040404040404040404010104040404040404040404040404040101030303030303030303030303030301010202020202020202020202020202010102020202020202020202020202020101010101010101010101010101010101`, img`
+            case "level1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020101010101010101010101010101020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202010101010101010101010101010102020101010101010101010101010101020201010101010101010101010101010202020202020202020202020202020202`, img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
@@ -90,7 +82,7 @@ namespace myTiles {
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+`, [myTiles.transparency16,sprites.dungeon.hazardWater,sprites.castle.tilePath5], TileScale.Sixteen);
         }
         return null;
     })
@@ -100,14 +92,6 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "myTile":
             case "tile1":return tile1;
-            case "myTile0":
-            case "tile2":return tile2;
-            case "myTile1":
-            case "tile3":return tile3;
-            case "myTile2":
-            case "tile4":return tile4;
-            case "myTile3":
-            case "tile5":return tile5;
         }
         return null;
     })
